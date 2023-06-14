6212c5dd80d7ba490caf14ebcb5b81f63810ae93
Michh <johnmichadasis@gmail.com>
2023-06-14 21:35:29 +0300

    Update 1.2.3b
    **Minor Changes**
    -Slash commands are now global(My fault I didn't know they were getting updated on my server only.)
    -Changed a variable ownerName: Michh#7658 --> michadasis (This is due to me getting the username update which I personally think is shit)

--- deploy-commands.js
+++ deploy-commands.js
@@ -19,9 +19,9 @@ const rest = new REST({ version: '10' }).setToken(`${process.env.TOKEN}`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
+	Routes.applicationCommands(clientId),
+	{ body: commands },
+);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
